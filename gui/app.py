e55b2fd7806161323653d2a22e326b8f55c29954
Kpatdu <kpatdu2@huskers.unl.edu>
2025-11-09 16:55:34 -0600

    Renamed all sub-folders for easier implementation
    Created a new sub-folder: gui/
    - Created app.py, rough outline of what application should do
    - Added an __init__.py for importing
    - Added styles.qss for easy editing with PyQt5
    
    Added __init__.py file to cv2_Python_Files

---
+++ gui/app.py
@@ -0,0 +1,109 @@
+import sys
+import os
+# This catches the file location for importing functions from the other python folders in the project
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+import cv2
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QStackedWidget, QMessageBox
+from PyQt5.QtGui import QDesktopServices
+from PyQt5.QtCore import Qt, QUrl
+
+#Layout on importing functions for use
+from cv2_Python_Files.cv2_Working_Example import run_camera
+
+class MainMenu(QWidget):
+    def __init__(self, stacked_widget):
+        super().__init__()
+        self.stacked_widget = stacked_widget
+        layout = QVBoxLayout()
+
+        #Title
+        title = QLabel("--- CSCI4250 HCI Camera Tracking Application ---")
+        title.setObjectName("title")
+        layout.addWidget(title)
+
+        #Start Button
+        start_button = QPushButton("Start")
+        start_button.setObjectName('start_button')
+        start_button.clicked.connect(lambda: self.stacked_widget.setCurrentIndex(1))
+        layout.addWidget(start_button)
+
+        #About Button will link to GitHub Repo
+        about_button = QPushButton("About")
+        about_button.setObjectName('about_button')
+        about_button.clicked.connect(lambda: QDesktopServices.openUrl(QUrl("https://github.com/ysabum/CSCI4250_HCI/tree/main")))
+        layout.addWidget(about_button)
+
+        #Exit will close the application
+        exit_button = QPushButton("Exit")
+        exit_button.setObjectName('exit_button')
+        exit_button.clicked.connect(QApplication.quit)
+        layout.addWidget(exit_button)
+
+        layout.addStretch()
+
+        self.setLayout(layout)
+
+class StartScreen(QWidget):
+    def __init__(self, stacked_widget):
+        super().__init__()
+        layout = QVBoxLayout()
+
+        # Title
+        title = QLabel("Choose a screen and enable the webcam?")
+        title.setObjectName("title")
+        layout.addWidget(title)
+
+        # Confirmation Button to launch program
+        confirm_button = QPushButton("Yes")
+        confirm_button.clicked.connect(self.enable_webcam)
+        layout.addWidget(confirm_button)
+
+        # Back button to navigate back to the main menu
+        back_button = QPushButton("Back")
+        back_button.clicked.connect(lambda: stacked_widget.setCurrentIndex(0))
+        layout.addWidget(back_button)
+
+        layout.addStretch()
+        self.setLayout(layout)
+
+    def enable_webcam(self):
+        # Placeholder for actual function to choose a screen and enable webcam.
+        # Imported a run_camera() function in cv2_Working_Example.py as a placeholder.
+        run_camera()
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PyQt App Shell")
+        layout = QVBoxLayout()
+
+        self.stacked_widget = QStackedWidget()
+
+        # Add Main Menu Index
+        self.main_menu = MainMenu(self.stacked_widget)
+        self.stacked_widget.addWidget(self.main_menu)
+
+        # Add Start Screen Index
+        self.start_screen = StartScreen(self.stacked_widget)
+        self.stacked_widget.addWidget(self.start_screen)
+
+        # Sets current Index
+        self.stacked_widget.setCurrentIndex(0)
+
+        layout.addWidget(self.stacked_widget)
+        self.setLayout(layout)
+
+def main():
+    # Main Script to run the Application
+    app = QApplication(sys.argv)
+
+    # Style Sheet Import!
+    with open(os.path.join(os.path.dirname(__file__), "styles.qss"), "r") as f:
+        app.setStyleSheet(f.read())
+
+    window = App()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
